5f8f00b3d9f0073396fc3fbc19776082c8d38efd
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-02-26 17:46:48 +0530

    Fix

--- lambda/util.js
+++ lambda/util.js
@@ -10,7 +10,7 @@ module.exports.getS3PreSignedUrl = function getS3PreSignedUrl(s3ObjectKey) {
     const s3PreSignedUrl = s3SigV4Client.getSignedUrl('getObject', {
         Bucket: bucketName,
         Key: s3ObjectKey,
-        Expires: 60*5 // the Expires is capped for 1 minute
+        Expires: 60 * 1 // the Expires is capped for 1 minute
     });
     console.log(`Util.s3PreSignedUrl: ${s3ObjectKey} URL ${s3PreSignedUrl}`);
     return s3PreSignedUrl;
